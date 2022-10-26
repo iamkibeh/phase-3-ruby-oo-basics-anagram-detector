60ef436d92a334f93a5bfaa7d3c4275927075eb9
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-10-26 23:49:50 +0300

    complete assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize word
+        @word = word.chars.sort
+    end
+    
+
+    def match arr
+        newArr = []
+        arr.each do |i|
+            sorted_char = i.chars.sort
+            sorted_char == @word && newArr << i
+        end
+        p newArr
+    end
+end
